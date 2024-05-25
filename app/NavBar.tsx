22b583047aec8e93197cfb4b187f4d5a44d7accb
brookedowney <brookelizabeth@live.com>
2024-05-25 08:47:06 -0500

    Style the active link in the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { SiDrupal } from "react-icons/si";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +22,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-600 hover:text-zinc-900 transition-colors"
+            className={classnames({
+              "text-zinc-950 font-bold": link.href === currentPath,
+              "text-zinc-600 font-normal": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
