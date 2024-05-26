c9ab79f00af258bca91898259ab5f25be166d021
brookedowney <brookelizabeth@live.com>
2024-05-26 16:09:21 -0500

    Fixed bug on IssueDetailPage

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/prisma/client";
+import { Heading } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -7,7 +8,6 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  if (typeof params.id !== "number") notFound();
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -16,7 +16,7 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
+      <Heading>{issue.title}</Heading>
       <p>{issue.description}</p>
       <p>{issue.status}</p>
       <p>{issue.createdAt.toDateString()}</p>
