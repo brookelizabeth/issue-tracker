91f3f22bbd7f3cd63f5a30fcfc148e919cb855a8
brookedowney <brookelizabeth@live.com>
2024-05-28 21:29:55 -0500

    Enhance the API to assign issues

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -3,5 +3,11 @@ import { z } from 'zod';
 //zod validation. Set min char req for title and desc.  Set max char req for title.  Error messaging.
 export const issueSchema = z.object({
     title: z.string().min(1, 'Title is required.').max(255),
-    description: z.string().min(1, 'Description is required')
+    description: z.string().min(1, 'Description is required').max(65535)
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, 'Title is required.').max(255).optional(),
+    description: z.string().min(1, 'Description is required').max(65535).optional(),
+    assignedToUserId: z.string().min(1, "AssignedToUserId is required.").max(255).optional().nullable()
 });
