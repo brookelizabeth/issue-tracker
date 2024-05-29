e1c132701741ab649b9d3944ee7f2929128f9b4f
brookedowney <brookelizabeth@live.com>
2024-05-29 11:41:24 -0500

    Sorting issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -34,10 +34,17 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
@@ -56,7 +63,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
                   {column.label}
                 </NextLink>
                 {column.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
+                  <ArrowUpIcon className="float-left" />
                 )}
               </Table.ColumnHeaderCell>
             ))}
